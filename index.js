4d1e59b2281374037c5c7ed29e4cf77fd6409f8a
Mateus Correia <mateuscorreia.dev@gmail.com>
2023-02-07 23:30:47 -0300

    added the function to the share button

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ shareBtn.addEventListener('click', () => {
     shareOptions.classList.toggle('active');
 })
 
+const whatsappBtn = document.querySelector(".whatsapp-btn");
+const instagramBtn = document.querySelector(".instagram-btn");
+const twitterBtn = document.querySelector(".twitter-btn");
+const linkedinBtn = document.querySelector(".linkedin-btn");
+
+function init() {
+    let postUrl = encodeURI(document.location.href);
+    let postTitle = encodeURI("Hi everyone, please check this out: ");
+
+    whatsappBtn.setAttribute(
+        "href",
+        `https://wa.me/?text=${postTitle} ${postUrl}`
+    );
+
+    twitterBtn.setAttribute(
+        "href",
+        `https://twitter.com/share?url=${postUrl}&text=${postTitle}` 
+    );
+}
+
+init()
