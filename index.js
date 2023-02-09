4559efa814abc70091da349fc23a78d78f2a6547
Mateus Correia <mateuscorreia.dev@gmail.com>
2023-02-08 22:08:44 -0300

    changed the share button

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-const shareBtn = document.querySelector('.share-btn');
-const shareOptions = document.querySelector('.share-options');
+const shareData = {
+    title: 'MDN',
+    text: 'Learn web development on MDN!',
+    url: 'https://developer.mozilla.org'
+  }
+  
+  const btn = document.querySelector('button');
+  const resultPara = document.querySelector('.result');
+  
+  // Share must be triggered by "user activation"
+  btn.addEventListener('click', async () => {
+    try {
+      await navigator.share(shareData);
+      resultPara.textContent = 'MDN shared successfully';
+    } catch (err) {
+      resultPara.textContent = `Error: ${err}`;
+    }
+  });
 
-shareBtn.addEventListener('click', () => {
-    shareOptions.classList.toggle('active');
-})
+// const shareBtn = document.querySelector('.share-btn');
+// const shareOptions = document.querySelector('.share-options');
 
-const whatsappBtn = document.querySelector(".whatsapp-btn");
-const instagramBtn = document.querySelector(".instagram-btn");
-const twitterBtn = document.querySelector(".twitter-btn");
-const linkedinBtn = document.querySelector(".linkedin-btn");
+// shareBtn.addEventListener('click', () => {
+//     shareOptions.classList.toggle('active');
+// })
 
-function init() {
-    let postUrl = encodeURI(document.location.href);
-    let postTitle = encodeURI("Hi everyone, please check this out: ");
+// const whatsappBtn = document.querySelector(".whatsapp-btn");
+// const instagramBtn = document.querySelector(".instagram-btn");
+// const twitterBtn = document.querySelector(".twitter-btn");
+// const linkedinBtn = document.querySelector(".linkedin-btn");
 
-    whatsappBtn.setAttribute(
-        "href",
-        `https://wa.me/?text=${postTitle} ${postUrl}`
-    );
 
-    twitterBtn.setAttribute(
-        "href",
-        `https://twitter.com/share?url=${postUrl}&text=${postTitle}` 
-    );
-}
 
-init()
+// function init() {
+//     let postUrl = encodeURI(document.location.href);
+//     let postTitle = encodeURI("Hi everyone, please check this out: ");
+
+//     whatsappBtn.setAttribute(
+//         "href",
+//         `https://wa.me/?text=${postTitle} ${postUrl}`
+//     );
+
+//     twitterBtn.setAttribute(
+//         "href",
+//         `https://twitter.com/share?url=${postUrl}&text=${postTitle}` 
+//     );
+// }
+
+// init()
