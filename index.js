6f56e45b2fc376e3a2200583705d1762cba4a71a
Mateus Correia <mateuscorreia.dev@gmail.com>
2023-02-14 20:57:58 -0300

    adjusted the share button function

--- index.js
+++ index.js
@@ -14,36 +14,6 @@ const shareData = {
       // resultPara.textContent = 'MDN shared successfully';
     } catch (err) {
       // resultPara.textContent = `Error: ${err}`;
-    }
+      shareDialog.classList.add('is-open');
+    } 
   });
-
-// const shareBtn = document.querySelector('.share-btn');
-// const shareOptions = document.querySelector('.share-options');
-
-// shareBtn.addEventListener('click', () => {
-//     shareOptions.classList.toggle('active');
-// })
-
-// const whatsappBtn = document.querySelector(".whatsapp-btn");
-// const instagramBtn = document.querySelector(".instagram-btn");
-// const twitterBtn = document.querySelector(".twitter-btn");
-// const linkedinBtn = document.querySelector(".linkedin-btn");
-
-
-
-// function init() {
-//     let postUrl = encodeURI(document.location.href);
-//     let postTitle = encodeURI("Hi everyone, please check this out: ");
-
-//     whatsappBtn.setAttribute(
-//         "href",
-//         `https://wa.me/?text=${postTitle} ${postUrl}`
-//     );
-
-//     twitterBtn.setAttribute(
-//         "href",
-//         `https://twitter.com/share?url=${postUrl}&text=${postTitle}` 
-//     );
-// }
-
-// init()
