3bc09d63785d86e4aa8c3653a3957fcb2cfc4fd4
Mateus Correia <mateuscorreia.dev@gmail.com>
2023-02-14 23:10:10 -0300

    adjusted the share button function

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-const shareData = {
-    title: 'Gabriel Cryslian',
-    text: 'Educação Musical & Produção',
-    url: 'https://gabrielcryslian.github.io/linktree/'
+// const shareData = {
+//     title: 'Gabriel Cryslian',
+//     text: 'Educação Musical & Produção',
+//     url: 'https://gabrielcryslian.github.io/linktree/'
+//   }
+  
+//   const btn = document.querySelector('button');
+//   const resultPara = document.querySelector('.result');
+  
+//   // Share must be triggered by "user activation"
+//   btn.addEventListener('click', async () => {
+//     try {
+//       await navigator.share(shareData);
+//       // resultPara.textContent = 'MDN shared successfully';
+//     } catch (err) {
+//       // resultPara.textContent = `Error: ${err}`;
+//       shareDialog.classList.add('is-open');
+//     } 
+//   });
+
+  function share(){
+    if (navigator.share !== undefined) {
+      navigator.share({
+        title: 'Gabriel Cryslian',
+        text: 'Educação Musical & Produção',
+        url: 'https://gabrielcryslian.github.io/linktree/'
+      })
+      .then(() => console.log('Successful share'))
+      .catch((error) => console.log('Error sharing', error));
+    }
   }
-  
-  const btn = document.querySelector('button');
-  const resultPara = document.querySelector('.result');
-  
-  // Share must be triggered by "user activation"
-  btn.addEventListener('click', async () => {
-    try {
-      await navigator.share(shareData);
-      // resultPara.textContent = 'MDN shared successfully';
-    } catch (err) {
-      // resultPara.textContent = `Error: ${err}`;
-      shareDialog.classList.add('is-open');
-    } 
-  });
