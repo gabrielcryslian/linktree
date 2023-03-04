ca25fe0003736aa5b7fcfeab2871a8c8dd8314f2
Mateus Correia <mateuscorreia.dev@gmail.com>
2023-03-04 12:30:51 -0300

    did some bug correction in the papypal button link

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-function share() {
-  if (navigator.share !== undefined) {
-    navigator
-      .share({
-        title: "Gabriel Cryslian",
-        text: "Educação Musical & Produção",
-        url: "https://gabrielcryslian.github.io/linktree/",
+// This is the share function
+  function share(){
+    if (navigator.share !== undefined) {
+      navigator.share({
+        title: 'Gabriel Cryslian',
+        text: 'Educação Musical & Produção',
+        url: 'https://gabrielcryslian.github.io/linktree/'
       })
-      .then(() => console.log("Successful share"))
-      .catch((error) => console.log("Error sharing", error));
+      .then(() => console.log('Successful share'))
+      .catch((error) => console.log('Error sharing', error));
+    }
   }
-}
 
-const tiktokDiv = document.getElementById("tiktok-div");
-const tagLinkTik = document.getElementById("tag-link-tt");
-const tiktokMainDiv = document.getElementById("div-link-tt");
-
-function toggleTiktok() {
-  if (tiktokDiv.style.display === "none") {
-    tiktokDiv.style.display = "block";
-  } else {
-    tiktokDiv.style.display = "none";
+  // This is the function about the tiktok embed link
+  const tiktokDiv = document.getElementById("tiktok-div");
+  
+  function toggleTiktok() {
+    if (tiktokDiv.style.display === "none") {
+      tiktokDiv.style.display = "block";
+    } else {
+      tiktokDiv.style.display = "none";
+    }
   }
-}
-
